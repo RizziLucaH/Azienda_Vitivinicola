250e84a9e1fe099a554f27b17973e6695ff5a62f
RizziLucaH <luca.rizzi.studenti@isii.it>
2023-03-31 09:43:15 +0200

    login e register login e register ufficialmente funzionanti, inizio session

--- login_act.php
+++ login_act.php
@@ -3,34 +3,25 @@ require("_config_inc.php");
 require("_db_dal_inc.php");
 
 
-
-if(array_key_exists('signup_nome',$_POST)){
-    $signup_nome=ucfirst($_POST['signup_nome']);//converte il primo carattere
-    //se non viene passato nessun campo vuol dire che sta cercando di accedere
-    if(array_key_exists('signup_piva',$_POST))
-    {
-
-        $signup_piva=$_POST['signup_piva'];
-        $signup_indirizzo=$_POST['signup_indirizzo'];
-    }
-
-}
-
 $email=$_POST['email'];
 $psw = $_POST['psw'];
 
 $conn=db_connect();
 
+if(array_key_exists('signup_nome',$_POST)){
+    $signup_nome=ucfirst($_POST['signup_nome']);//converte il primo carattere
+    //se non viene passato nessun campo vuol dire che sta cercando di accedere
 
-if(array_key_exists('signup_nome',$_POST))
-{
-    //SE ESISTE IL NOME L'UTENTE SI STA REGISTRANDO
+    $signup_piva=$_POST['signup_piva'] | "NULL";
+    $signup_indirizzo=$_POST['signup_indirizzo'] | "NULL";
+
     new_user($conn,$signup_nome,$email,$signup_piva,$signup_indirizzo,$psw);
     header("location: login.php");
 }
 else
 {
     //SE NON ESISTE IL NOME L'UTENTE STA CERCANDO DI ACCEDERE
+    
     
     $result=verifica_user($conn,$email,$psw);
     if(empty($result))
@@ -41,11 +32,11 @@ else
     else
     {
         //SE è GIUSTO AVVIA LA SESSION, CONSENTENDO COSI DI ACCEDERE ALLE PAGINE PRIVATE
-        session_start();
-        $_SESSION['email'] = $email;
-        $_SESSION['psw'] = $psw;
-        $_SESSION['nome'] = $result['nome'];
-        $_SESSION['piva'] = $result['piva'];
+        // session_start();
+        // $_SESSION['email'] = $email;
+        // $_SESSION['psw'] = $psw;
+        // $_SESSION['nome'] = $result['nome'];
+        // $_SESSION['piva'] = $result['piva'];
         // $_SESSION['filtro'] = ' ';
         
         header("location: index.php");
