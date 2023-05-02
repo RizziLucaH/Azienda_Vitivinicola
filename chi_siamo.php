badab85505eebe612ab5c7357b6bc3ce6e0b4974
TomPaglia <sterconialvezio@gmail.con>
2023-05-02 23:07:40 +0200

    Inizio della svperba pagina chi siamo

--- chi_siamo.php
+++ chi_siamo.php
@@ -9,7 +9,41 @@ include('header_inc.php');
 
 
 	<div >
+		<link rel="stylesheet" href="style/styleChiSiamo.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
 
+		<section class="hero-section">
+		<div class="card-grid">
+			<a class="card" href="#">
+			<div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1557177324-56c542165309?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80)"></div>
+			<div class="card__content">
+				<p class="card__category">Project Manager</p>
+				<h3 class="card__heading">Lvca Il Brillante</h3>
+			</div>
+			</a>
+			<a class="card" href="#">
+			<div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1557187666-4fd70cf76254?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)"></div>
+			<div class="card__content">
+				<p class="card__category">Segretario del P.M.</p>
+				<h3 class="card__heading">Tommaso Il Svperbo</h3>
+			</div>
+			</a>
+			<a class="card" href="#">
+			<div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1556680262-9990363a3e6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)"></div>
+			<div class="card__content">
+				<p class="card__category">Minatore Illvstre</p>
+				<h3 class="card__heading">Davide Il Bovaro</h3>
+			</div>
+			</li>
+			<a class="card" href="#">
+			<div class="card__background" style="background-image: url(https://images.unsplash.com/photo-1557004396-66e4174d7bf6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)"></div>
+			<div class="card__content">
+				<p class="card__category">Decimiciatore</p>
+				<h3 class="card__heading">Nicolas Il Trascvrato</h3>
+			</div>
+			</a>
+		<div>
+		</section>
 	</div>
 
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
